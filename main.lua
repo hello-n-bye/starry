d50fecf24ece3005fc352bb3a6d8434ad0ceab4e
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-22 23:35:33 -0800

    Test to see if it works

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-local export = ''
+local export = 'src/checker.lua'
 
-loadstring(game:HttpGetAsync('https://github.com/hello-n-bye/starry/' .. export, true))()
+loadstring(game:HttpGetAsync('https://github.com/hello-n-bye/starry/' .. export, true))() -- Test
