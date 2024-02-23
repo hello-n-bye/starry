108b5eb1fbafee811e0986726f6aac8ce7346408
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-22 23:38:29 -0800

    Add Selene

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 local export = 'src/checker.lua'
 
-loadstring(game:HttpGetAsync('https://github.com/hello-n-bye/starry/' .. export, true))() -- Test
+loadstring(game:HttpGetAsync('https://github.com/hello-n-bye/starry/' .. export, true))()
