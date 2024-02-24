cf92942515fac34f69758fdcce4cf97b54f54789
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-23 18:11:41 -0800

    Pet Sim 99 auto-farm

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@ local succ, err = xpcall(function()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/src/checker.lua", true))()
 
     print('loaded ' .. getgenv().script)
-end, function()
-    warn('there is an error when loading')
+end, function(code)
+    warn('code execution failure: ' .. code)
 end)
