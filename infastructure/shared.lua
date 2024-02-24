0ba2081e7d5579a911a82e257d12d5a030638989
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-23 18:18:50 -0800

    Pet Sim 99 auto-farm

--- infastructure/shared.lua
+++ infastructure/shared.lua
@@ -1,5 +1,4 @@
 _G.__zones = true
 _G.__farming = true
-_G.__ver = "1.0"
 
 print('loaded globals')
--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local succ, err = xpcall(function()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/src/checker.lua", true))()
 
-    print('loaded ' .. tostring(_G.__ver))
+    print('loaded fully')
 end, function(code)
     warn('code execution failure: ' .. code)
 end)
