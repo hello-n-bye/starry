26df970b9adb075b777680f8903ba49b90740c6a
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-23 18:40:01 -0800

    Pet Sim 99 auto-farm

--- games/pSim99.lua
+++ games/pSim99.lua
@@ -17,43 +17,55 @@ local localPlayer = players.LocalPlayer
 local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
 local humanoidRP = character.HumanoidRootPart or character:WaitForChild("HumanoidRootPart")
 
-__game.infiniteSpeed()
+lib.PlayerPet.CalculateSpeedMultiplier = __game.infiniteSpeed()
 
-while (states.farming) do
-    functions.yield()
-
-    local zone, info = lib["ZoneCmds"].GetMaxOwnedZone()
-    local zoning, nexts = lib["ZoneCmds"].GetNextZone()
-
-    if (states.__zones) then
-        if (zoning) and (nexts) then
-            if (balancing.CalcGatePrice(nexts)) <= (lib["CurrencyCmds"].Get("Coins")) then
-                network.Invoke("Zones_RequestPurchase", nexts.ZoneName)
+coroutine.wrap(function()
+    while (states.farming) do
+        functions.yield()
+    
+        local zone, info = lib["ZoneCmds"].GetMaxOwnedZone()
+        local zoning, nexts = lib["ZoneCmds"].GetNextZone()
+    
+        if (states.__zones) then
+            if (zoning) and (nexts) then
+                if (balancing.CalcGatePrice(nexts)) <= (lib["CurrencyCmds"].Get("Coins")) then
+                    network.Invoke("Zones_RequestPurchase", nexts.ZoneName)
+                end
+            end
+        end
+    
+        if (lib["MapCmds"].GetCurrentZone()) ~= (info._id) or not (lib["MapCmds"].IsInDottedBox()) then
+            if (info.ZoneFolder:FindFirstChild("INTERACT")) then
+                local max, breakZone
+    
+                for _,v in ipairs(info.ZoneFolder.INTERACT.BREAK_ZONES:GetChildren()) do
+                    if not (max) then
+                        max = v.Size.X * v.Size.Y * v.Size.Z
+                    end if not (breakZone) then
+                        breakZone = v
+                    end
+    
+                    if (max) < (v.Size.X * v.Size.Y * v.Size.Z) then
+                        breakZone = v
+                    end
+                end
+    
+                humanoidRP.CFrame = breakZone.CFrame * CFrame.new(0, 10, 0)
+            else
+                humanoidRP.CFrame = info.ZoneFolder.PERSISTENT.Teleport.CFrame
             end
         end
     end
+end)()
 
-    if (lib["MapCmds"].GetCurrentZone()) ~= (info._id) or not (lib["MapCmds"].IsInDottedBox()) then
-        if (info.ZoneFolder:FindFirstChild("INTERACT")) then
-            local max, breakZone
+coroutine.wrap(function()
+    while (functions.yield()) do
+        local breakable = getNearestBreakable()
 
-            for _,v in ipairs(info.ZoneFolder.INTERACT.BREAK_ZONES:GetChildren()) do
-                if not (max) then
-                    max = v.Size.X * v.Size.Y * v.Size.Z
-                end if not (breakZone) then
-                    breakZone = v
-                end
-
-                if (max) < (v.Size.X * v.Size.Y * v.Size.Z) then
-                    breakZone = v
-                end
-            end
-
-            humanoidRP.CFrame = breakZone.CFrame * CFrame.new(0, 10, 0)
-        else
-            humanoidRP.CFrame = info.ZoneFolder.PERSISTENT.Teleport.CFrame
+        if (breakable) then
+            Network.Fire("Breakables_PlayerDealDamage", breakable.Name)
         end
     end
-end
+end)()
 
 print('loaded pet sim hax')
