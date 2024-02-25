5827817823222e0ad862b01865dc7cfce36bb3fa
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-24 18:41:00 -0800

    UI in-dev

--- src/checker.lua
+++ src/checker.lua
@@ -2,7 +2,14 @@ local ids = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hell
 
 local placeid = game.PlaceId
 
-if ((placeid) == ids.pSim99) then
-    print('loaded pet sim')
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/pSim99.lua"))()
+local options = { dev = true }
+
+if (options.dev) then
+    print('loaded dev ui')
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/.dev/ui.lua"))()
+else
+    if ((placeid) == ids.pSim99) then
+        print('loaded pet sim')
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/pSim99.lua"))()
+    end
 end
