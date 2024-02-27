1a3e561c1964cf3b2c6d0ccc343e60394b9b9de7
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-27 00:29:31 -0800

    Lobby deprecation (only load in-game)

--- src/checker.lua
+++ src/checker.lua
@@ -5,7 +5,11 @@ local placeid = game.PlaceId
 if (placeid) == 13864661000 or (placeid) == 13864667823 then
     -- break in 2
 
-    return (loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/break-in-2.lua"))())
+    if (placeid) == 13864661000 then
+        return (print("💫 Starry: You must be in a game to use the script!"))
+    else
+        return (loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/break-in-2.lua"))())
+    end
 else
     return (print("💫 Starry: Sorry, we don't support this game."))
 end
