7ae643e7b115a2479050f5a0a7acb8f3513141e2
Logan Makris <134660747+Suno0526@users.noreply.github.com>
2024-02-27 19:15:35 -0800

    Add full detection for PC and Mobile

--- src/checker.lua
+++ src/checker.lua
@@ -3,17 +3,21 @@ local devices = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/
 local placeid = game.PlaceId
 
 local function isMobile()
-    
+    if (devices) == "PC" then
+        return "pc"
+    else
+        return "mobile"
+    end
 end
 
 if (placeid) == 13864661000 or (placeid) == 13864667823 then
-    -- break in 2
-
     if (placeid) == 13864661000 then
         return (print("💫 Starry: You must be in a game to use the script!"))
     else
-        
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/breakIn/" .. isMobile() ..".lua", true))()
     end
 else
-    return (print("💫 Starry: Sorry, we don't support this game."))
+    print("💫 Starry: Game not found, loaded universal.")
+
+    return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/games/uni/" .. isMobile() ..".lua", true))()
 end
