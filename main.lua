fec14982720c3104591f467578b04733ffc2b1aa
Headlined <159689944+hello-n-bye@users.noreply.github.com>
2024-03-10 23:55:56 -0700

    Modify debug handler 🐛

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@ local succ, err = xpcall(function()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/src/checker.lua", true))()
 
     print("Loaded, with 0 problems. ⭐")
-end, function(code)
-    warn("Code failure: " .. code)
+end, function(err)
+    warn("Starry Issued @ " .. err)
 end)
