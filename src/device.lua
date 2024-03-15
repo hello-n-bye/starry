337c0e07d762d83891371d096d5b9e7f278e1b83
💫 Suno <159689944+hello-n-bye@users.noreply.github.com>
2024-03-15 01:36:10 -0700

    Emulator detection added. 🎉

--- src/device.lua
+++ src/device.lua
@@ -1,7 +1,62 @@
-local userInputService = game:GetService("UserInputService")
+-- a LOT better than detecting with userInputService
+-- this can detect emulators since the ui appears a lot bigger on there.
 
-if (userInputService.TouchEnabled) and not (userInputService.MouseEnabled) then
-    return "Mobile"
-else
-    return "PC"
+local endpoint = "https://httpbin.org/user-agent"
+local api = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/src/api.lua", true))()
+
+local http = game:GetService("HttpService")
+
+local req = request or (fluxus and fluxus.request)
+
+local function find()
+    local res = req({
+        Url = endpoint,
+        Method = "GET"
+    })
+
+    return res.Body
 end
+
+local function parse(res)
+    local agent = http:JSONDecode(res)
+
+    if (agent) and (agent["user-agent"]) then
+        return agent["user-agent"]
+    else
+        api.warn("Invalid user-agent information")
+        
+        return nil
+    end
+end
+
+local function platform(agent)
+    if (string.find(agent, "Android")) then
+        return "Android"
+    elseif (string.find(agent, "iOS")) then
+        return "iOS"
+    elseif (string.find(agent, "Windows")) or (string.find(agent, "Krampus")) then
+        return "Windows"
+    else
+        api.log(agent)
+    end
+end
+
+local function main()
+    local agent = find()
+
+    if (agent) then
+        local platform = parse(agent)
+    
+        if (platform) then
+            api.log("Connected with " .. platform)
+        else
+            api.warn("Failed to determine platform from user-agent. Defaulting to Mobile.")
+            return "Android"
+        end
+    else
+        api.warn("Failed to fetch user-agent information. Defaulting to Mobile.")
+        return "Android"
+    end
+end
+
+return main()
