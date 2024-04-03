85f0f2958da91834eeb8a30f3fe15b3c4ac4fc4e
suno <159689944+hello-n-bye@users.noreply.github.com>
2024-04-02 21:03:31 -0700

    Removed useless warn 🐋

--- main.lua
+++ main.lua
@@ -14,41 +14,12 @@ end, function(err)
     if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
         flu:Notify({
             Title = "💫  Starry Can't Start.",
-            Content = "Please look inside your console. Use /console, or press F9 to view it.",
+            Content = "The script is currently down, please try again later.",
             Duration = 5
         })
 
         if (setclipboard) then
             setclipboard("https://github.com/hello-n-bye/starry")
         end
-        
-        warn([[
-        
-        
-    | Hmm.. Something went wrong.
-
-      ___                         ___           ___           ___                 
-     /\__\                       /\  \         /\  \         /\  \                
-    /:/ _/_         ___         /::\  \       /::\  \       /::\  \         ___   
-   /:/ /\  \       /\__\       /:/\:\  \     /:/\:\__\     /:/\:\__\       /|  |  
-  /:/ /::\  \     /:/  /      /:/ /::\  \   /:/ /:/  /    /:/ /:/  /      |:|  |  
- /:/_/:/\:\__\   /:/__/      /:/_/:/\:\__\ /:/_/:/__/___ /:/_/:/__/___    |:|  |  
- \:\/:/ /:/  /  /::\  \      \:\/:/  \/__/ \:\/:::::/  / \:\/:::::/  /  __|:|__|  
-  \::/ /:/  /  /:/\:\  \      \::/__/       \::/~~/~~~~   \::/~~/~~~~  /::::\  \  
-   \/_/:/  /   \/__\:\  \      \:\  \        \:\~~\        \:\~~\      ~~~~\:\  \ 
-     /:/  /         \:\__\      \:\__\        \:\__\        \:\__\          \:\__\
-     \/__/           \/__/       \/__/         \/__/         \/__/           \/__/
-
-
-    | Starry 💫 is currently down for maintenance.
-    | Check our official GitHub repository for updates, or join our server!
-    
-    | github.com/hello-n-bye/starry
-    | discord.gg/bMmZHj2KVp
-    
-    | P.S. our GitHub repository has been copied to your clipboard.
-            ]])
-    else
-        warn("💫 Starry Debugger: " .. err)
     end
 end)
