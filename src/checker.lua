06b55463a8d1a51ed9bc127f3be3f6a640ae26db
suno <159689944+hello-n-bye@users.noreply.github.com>
2024-04-25 09:23:03 -0700

    Preparing for incoming game

--- src/checker.lua
+++ src/checker.lua
@@ -1,4 +1,10 @@
-local devices = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/dev/src/device.lua", true))()
+local devices = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/master/src/device.lua", true))()
+
+-- Change this data if you're using a fork.
+local githubData = {
+    ["branch"] = "master",
+    ["owner"] = "hello-n-bye"
+}
 
 local placeid = game.PlaceId
 
@@ -12,8 +18,10 @@ end
 
 if (placeid) == 13864661000 or (placeid) == 13864667823 then
     if (placeid) == 13864661000 then
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/dev/games/breakIn/lobby/" .. isMobile() ..".lua", true))()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/" .. githubData.owner .. "/starry/" .. githubData.branch .. "/games/breakIn/lobby/" .. isMobile() ..".lua", true))()
     else
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/hello-n-bye/starry/dev/games/breakIn/" .. isMobile() ..".lua", true))()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/" .. githubData.owner .. "/starry/" .. githubData.branch .. "/games/breakIn/" .. isMobile() ..".lua", true))()
     end
+elseif (placeid) == 738339342 then
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/" .. githubData.owner .. "/starry/" .. githubData.branch .. "/games/floodEscape" .. isMobile() .. ".lua", true))()
 end
