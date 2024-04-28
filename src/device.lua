0e3ae147c1a88ddd362f62fa6394c4efcb5e66d0
suno <159689944+hello-n-bye@users.noreply.github.com>
2024-04-28 04:57:39 -0700

    Remove "Windows" handler

--- src/device.lua
+++ src/device.lua
@@ -34,7 +34,7 @@ local function platform(agent)
         return "Android"
     elseif (string.find(agent, "iOS")) then
         return "iOS"
-    elseif (string.find(agent, "Windows")) or (string.find(agent, "Krampus")) or (string.find(identifyexecutor(), "Wave")) then
+    elseif (string.find(agent, "Krampus")) or (string.find(identifyexecutor(), "Wave")) then
         return "Windows"
     else
         return "Android" -- failsafe for unknown device
